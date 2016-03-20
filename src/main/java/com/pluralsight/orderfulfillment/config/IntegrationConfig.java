8e14a537c7f369c244e849ff457ecb07d44f5f13
Ravi Ved <ravi.ved@gmail.com>
2016-03-20 14:15:37 +0530

    adding integration config and update pom for camel jars

---
+++ src/main/java/com/pluralsight/orderfulfillment/config/IntegrationConfig.java
@@ -0,0 +1,55 @@
+package com.pluralsight.orderfulfillment.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.spring.javaconfig.CamelConfiguration;
+import org.springframework.core.env.Environment;
+
+@org.springframework.context.annotation.Configuration
+public class IntegrationConfig extends CamelConfiguration {
+
+	
+	
+	@Inject
+	private Environment environment;
+	
+	@Override
+	public List<RouteBuilder> routes(){
+		
+		List<RouteBuilder> routeList = new ArrayList<RouteBuilder>();
+		
+		
+		routeList.add(new RouteBuilder() {
+			
+			@Override
+			public void configure() throws Exception{
+				
+				from(
+						"file://"
+								+ environment
+									.getProperty("order.fulfillment.center.1.outbound.folder") 
+									+ "?noop=true")
+				.to("file://"
+						+ environment
+							.getProperty("order.fulfillment.center.1.outbound.folder")
+						+ "/test");
+						
+				
+			}	
+				
+			
+		});
+		
+		
+		return routeList;
+	}
+	
+	
+	
+	
+	
+}
